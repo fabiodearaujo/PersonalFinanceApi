b66653278ceee313281f0bf9d95d311b223ea11b
Fabio De Araujo <fabiodearaujo@outlook.com>
2025-10-30 01:00:35 +0000

    formated users one more time

--- app/routers/users.py
+++ app/routers/users.py
@@ -21,8 +21,10 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # check if password is strong enough
     if not utils.check_password_strength(user.password):
         return {
-            "error": ("Password is not strong enough. (Minimum of 8 characters, "
-                     "upper and lower case, number and a special symbol.)")
+            "error": (
+                "Password is not strong enough. (Minimum of 8 characters, "
+                "upper and lower case, number and a special symbol.)"
+            )
         }, status.HTTP_400_BAD_REQUEST
 
     # hash the password
@@ -136,8 +138,10 @@ def update_user_password(
     # check if password is strong enough
     if not utils.check_password_strength(user.new_password):
         return {
-            "error": ("Password is not strong enough. (Minimum of 8 characters,"
-                     " upper and lower case, number and a special symbol.)")
+            "error": (
+                "Password is not strong enough. (Minimum of 8 characters,"
+                " upper and lower case, number and a special symbol.)"
+            )
         }, status.HTTP_400_BAD_REQUEST
 
     # return user details
